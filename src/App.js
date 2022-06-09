4df13346ac5400880e7f9fdd8be881ae0cdfad1f
panosjapan7 <panosjapan@gmail.com>
2022-06-09 09:50:02 +0200

    Contact Landlord page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from "./pages/SignUp";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
